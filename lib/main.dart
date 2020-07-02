f50db29f03a8a204a893528ac9e7d618e591cb08
David Vergara <david.l.vergara@gmail.com>
2020-07-02 15:38:30 -0500

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -42,15 +42,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
   final providers = new List(3);
 
@@ -290,8 +281,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        tooltip: 'Refresh',
+        child: Icon(Icons.refresh),
       ),
     );
   }
